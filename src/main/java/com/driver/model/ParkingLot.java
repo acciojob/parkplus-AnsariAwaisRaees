36979d2ef46e141e17594e2fdd3a315b6a1ab605
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-10-01 14:25:04 +0530

    Update ParkingLot.java

--- src/main/java/com/driver/model/ParkingLot.java
+++ src/main/java/com/driver/model/ParkingLot.java
@@ -1,28 +1,30 @@
 package com.driver.model;
 
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Table(name="parkingLot")
 public class ParkingLot {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String name;
+
+    private  String name;
+
     private String address;
-    @OneToMany(mappedBy ="parkingLot",cascade=CascadeType.ALL)
-    List<Spot> spotList;
 
-    public ParkingLot() {
-    }
+    @OneToMany(mappedBy = "parkingLot",cascade = CascadeType.ALL)
+    List<Spot> spotList = new ArrayList<>();
 
-    public ParkingLot(int id, String name, String address, List<Spot> spotList) {
-        this.id = id;
+    public ParkingLot(String name, String address) {
         this.name = name;
         this.address = address;
-        this.spotList = new ArrayList<>();
+    }
+
+    public ParkingLot() {
     }
 
     public int getId() {
