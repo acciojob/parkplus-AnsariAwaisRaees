f02ffe42c023d57dd105a908e14be25a06dcfa50
DELL <ansariawaisraees@gamil.com>
2023-10-02 12:10:20 +0530

    Adding Solution

--- src/main/java/com/driver/ParkPlus.java
+++ src/main/java/com/driver/ParkPlus.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class ParkPlus {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ParkPlus.class, args);
 	}
